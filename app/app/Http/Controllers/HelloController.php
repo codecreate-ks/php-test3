87350f90655a8c9f0553608f3f312ba56fbb6456
k.shogaki <style.sk88@gmail.com>
2019-11-22 20:56:59 +0900

    p66 リスト3-10

--- app/app/Http/Controllers/HelloController.php
+++ app/app/Http/Controllers/HelloController.php
@@ -8,37 +8,12 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
 
-    public function index(Request $request, Response $response){
-
-return <<<EOF
-<html>
-<head>
-<title>Hello/Index</title>
-<style>
-    body {
-    font-size: 16px;
-    color: #999;
+    public function index($id='zero'){
+        $data = [
+            'msg'=>'これはコントローラから渡されたメッセージです',
+            'id'=>$id
+        ];
+        return view('hello.index', $data);
     }
-    h1 {
-    font-size: 120px;
-    text-align: right;
-    color: #fafafa;
-    margin: 0px 0px -120px 0px;
-    }
-</style>
-</head>
-<body>
-    <h1>Hello</h1>
-    <h3>Request</h3>
-    <pre>{$request}</pre>
-    <h3>Response</h3>
-    <pre>{$response}</pre>
-</body>
-</html>
-EOF;
 
-        $response->setContent($html);
-        return $response;
-
-    }
 }
