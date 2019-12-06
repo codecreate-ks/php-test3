6caffd9aa27d000aa56db90e30a3d5a6dd3e7f1a
k.shogaki <style.sk88@gmail.com>
2019-12-06 17:59:15 +0900

    20191206_p94

--- app/resources/views/hello/index.blade.php
+++ app/resources/views/hello/index.blade.php
@@ -1,31 +1,27 @@
-<html>
-<head>
-    <title>Hello/Index</title>
-    <style>
-        body {
-            font-size: 16px;
-            color: #999;
-        }
-        h1 {
-            font-size: 120px;
-            text-align: right;
-            color: #f6f6f6;
-            margin: 0px 0px -120px 0px;
-        }
-    </style>
-</head>
-<body>
-    <h1>Blade/Index</h1>
-    <ol>
-    @php
-    $counter = 0;
-    @endphp
-    @while ($counter < count($data))
-        <li>{{$data[$counter]}}</li>
-    @php
-    $counter++;
-    @endphp
-    @endwhile
-    </ol>
-</body>
-</html>
+@extends('layouts.helloapp')
+
+@section('title','Index')
+
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <p>ここが本文のコンテンツです。</p>
+    <p>必要なだけ記述できます</p>
+
+    @component('components.message')
+        @slot('msg_title')
+        CAUTION!
+        @endslot
+
+        @slot('msg_content')
+        これはメッセージの表示です。
+        @endslot
+        @endcomponent
+@endsection
+
+@section('footer')
+copyright 2020 xxx.
+@endsection
---
+++ app/resources/views/layouts/helloapp.blade.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+    <title>@yield('title')</title>
+    <style>
+        body{
+            font-size: 18px;
+            color: #999;
+            margin: 5px;
+        }
+        h1{
+            font-size: 50px;
+            text-align: right;
+            color: #f6f6f6;
+            margin: -20px 0 -30px 0;
+        }
+        ul{
+            font-size: 14px;
+        }
+        hr{
+            margin: 25px 100px;
+            border-top: 1px dashed #ddd;
+        }
+        .menutitle{
+            font-size: 16px;
+            font-weight: bold;
+            margin: 0;
+        }
+        .content{
+            margin: 10px;
+        }
+        .footer{
+            text-align: right;
+            font-size: 12px;
+            margin: 10px;
+            border-bottom: 1px #ccc;
+            color: #ccc;
+        }
+    </style>
+</head>
+<body>
+    <h1>@yield('title')</h1>
+    @section('menubar')
+    <ul>
+        <p class="menutitle">※メニュー</p>
+        <li>@show</li>
+    </ul>
+    <hr size="1">
+    <main class="content">
+        @yield('content')
+    </main>
+    <footer class="footer">
+        @yield('footer')
+    </footer>
+</body>
+</html>
