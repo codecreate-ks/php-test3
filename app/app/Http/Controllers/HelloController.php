823352ab7656f2069846c83d7e94d61dfb23adfc
k.shogaki <style.sk88@gmail.com>
2019-12-06 15:59:02 +0900

    20191206_p79

--- app/app/Http/Controllers/HelloController.php
+++ app/app/Http/Controllers/HelloController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
+/* 〜p72
 class HelloController extends Controller
 {
     public function index(){
@@ -20,5 +21,18 @@ class HelloController extends Controller
             'msg'=>'こんにちは、' . $msg . 'さん！'
         ];
         return view('hello.index', $data);
+    }
+}
+*/
+
+// p73〜
+class HelloController extends Controller
+{
+    public function index(){
+        return view('hello.index');
+    }
+
+    public function post(Request $request){
+        return view('hello.index', ['msg'=>$request->msg]);
     }
 }
--- app/resources/views/hello/index.blade.php
+++ app/resources/views/hello/index.blade.php
@@ -16,7 +16,11 @@
 </head>
 <body>
     <h1>Blade/Index</h1>
-    <p>{{$msg}}</p>
+    @isset ($msg)
+    <p>こんにちは、{{$msg}}さん。</p>
+    @else
+    <p>何か書いてください。</p>
+    @endisset
     <form method="POST" action="/hello">
         {{ csrf_field() }}
         <input type="text" name="msg">
