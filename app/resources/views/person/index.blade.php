ade46a09906f639f0c1f1c149ba8628ba5183d09
k.shogaki <style.sk88@gmail.com>
2019-12-08 17:29:11 +0900

    20191208_p232

--- app/resources/views/person/index.blade.php
+++ app/resources/views/person/index.blade.php
@@ -10,12 +10,10 @@
 @section('content')
     <?php var_dump($items) ?>
     <table>
-    <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+    <tr><th>Data</th></tr>
     @foreach ($items as $item)
         <tr>
-            <td>{{$item->name}}</td>
-            <td>{{$item->mail}}</td>
-            <td>{{$item->age}}</td>
+            <td>{{$item->getData()}}</td>
         </tr>
     @endforeach
     </table>
