644385327c0b9b86fff262665b8f759739d9c908
k.shogaki <style.sk88@gmail.com>
2019-12-09 18:10:10 +0900

    p294

--- app/routes/web.php
+++ app/routes/web.php
@@ -54,3 +54,5 @@ Route::post('person/del', 'PersonController@remove');
 Route::get('board', 'BoardController@index');
 Route::get('board/add', 'BoardController@add');
 Route::post('board/add', 'BoardController@create');
+
+Route::resource('rest', 'RestappController');
